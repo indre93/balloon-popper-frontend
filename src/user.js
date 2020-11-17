39e8ae6b8df10841b53508c552d82200b0c606f7
Indre Bakeer <indre.bakeer@gmail.com>
2020-11-17 11:17:35 -0600

    remove unnecessary code

--- src/user.js
+++ src/user.js
@@ -3,15 +3,4 @@ class User {
     this.id = id;
     this.username = username;
   }
-
-  // instance method that will render the object to the DOM
-  renderUser() {
-    const usersDiv = document.getElementById('top-scores');
-    const ul = document.createElement('ul');
-    const li = document.createElement('li');
-
-    usersDiv.append(ul);
-    ul.append(li);
-    li.innerHTML += `${this.username}`;
-  }
 }
