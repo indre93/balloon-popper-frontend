05d4dabbfbf4963342b082124f7ca3dae1d20f59
Indre Bakeer <indre.bakeer@gmail.com>
2020-11-20 01:37:24 -0600

    include created_at property w getter function to format date and add method to display top games

--- src/game.js
+++ src/game.js
@@ -1,12 +1,33 @@
 class Game {
-  constructor(id, username, score) {
+  constructor(id, username, score, created_at) {
     this.id = id;
     this.username = username;
     this.score = score;
+    this.created_at = created_at;
   }
 
-  renderGame() {
-    const gamesContainer = document.getElementById('games-container');
-    gamesContainer.innerHTML = 'My game is here';
+  get datePlayed() {
+    let formatDate = new Date(`${this.created_at}`);
+    let date = formatDate.toDateString();
+    return date;
   }
+
+  renderTopGames() {
+    const tBody = document.getElementById("table-body");
+    let tr = document.createElement("tr");
+    tr.id = `${this.score}`;
+
+    if (tBody.hasChildNodes()) {
+      (tBody.firstChild.id < this.score) ? tBody.prepend(tr) : tBody.append(tr);
+    }
+    else {
+      tBody.prepend(tr);
+    }
+
+    tr.insertAdjacentHTML('afterbegin',
+      `<td>${this.username}</td>
+      <td>${this.score}</td>
+      <td>${this.datePlayed}</td>`);
+  }
+
 }
