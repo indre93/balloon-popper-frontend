4f4583cbb3ff0e7b269c4484b627cde15a08d71a
Indre Bakeer <indre.bakeer@gmail.com>
2020-11-28 03:42:13 -0600

    add checForRowMatching() & checkForColumnMatching() functions to remove balloons when colors match. Also refactor code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const BASE_URL = "http://localhost:3000";
 const gameContainer = document.getElementById("game-container");
 const colors = ["blue", "green", "orange", "purple", "red", "yellow"];
-let scoreCount = document.getElementById("score-count");
-let balloons = [];
+const rowWidth = 10;
+let balloonsArray = Array.from(gameContainer.children);
 let balloonDragged;
 let balloonReplaced;
 let balloonDraggedId;
 let balloonReplacedId;
 let twoMinutes = 60 * 2;
-let score = 1;
+let score = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
   fetchAndLoadUsers();
@@ -58,9 +58,98 @@ function pickRandomBalloon() {
   return balloon;
 }
 
+function renderBalloons() {
+  for (let i = 0; gameContainer.childElementCount < 40; i++) {
+    balloon = pickRandomBalloon();
+    balloon.id = i;
+    balloonsArray.push(balloon);
+  }
+
+  balloonsArray.forEach((balloon) => {
+    balloon.addEventListener("dragstart", dragStart);
+    balloon.addEventListener("dragenter", (e) => { e.preventDefault(); });
+    balloon.addEventListener("dragover", (e) => { e.preventDefault(); });
+    balloon.addEventListener("drop", dragDrop);
+  });
+
+  setInterval(() => {
+    checkForRowMatching();
+    checkForColumnMatching();
+  }, 100);
+}
+
+// occurs when the user starts to drag an element
+function dragStart(e) {
+  balloonDragged = e.target.src;
+  balloonDraggedId = balloonsArray.indexOf(e.target);
+}
+
+// occurs when the dragged element is dropped on the drop target
+function dragDrop(e) {
+  balloonReplaced = e.target.src;
+  balloonReplacedId = balloonsArray.indexOf(e.target);
+  validMove();
+}
+
+function validMove() {
+  let validMoves = [
+    balloonDraggedId - rowWidth,
+    balloonDraggedId - 1,
+    balloonDraggedId + 1,
+    balloonDraggedId + rowWidth
+  ];
+  let validMove = validMoves.includes(balloonReplacedId);
+
+  if (balloonReplacedId && validMove) {
+    balloonsArray[balloonReplacedId].src = balloonDragged;
+    balloonsArray[balloonDraggedId].src = balloonReplaced;
+  }
+  else if (balloonReplacedId && !validMove) {
+    balloonsArray[balloonReplacedId].src = balloonReplaced;
+    balloonsArray[balloonDraggedId].src = balloonDragged;
+  }
+  else balloonsArray[balloonDraggedId].src = balloonDragged;
+}
+
+function checkForRowMatching() {
+  for (i = 0; i < 37; i++) {
+    let matchingBalloon = balloonsArray[i].src;
+    let possibleRowMatch = [
+      [i, i + 1, i + 2],
+      [i, i + 1, i + 2, i + 3],
+      [i, i + 1, i + 2, i + 3, i + 4]
+    ];
+
+    // Do not check the last two balloons of each row for matching since we need at least three per row
+    const notValid = [8, 9, 18, 19, 28, 29];
+    if (notValid.includes(i)) continue;
+
+    if (possibleRowMatch[2].every(index => balloonsArray[index].src === matchingBalloon)) {
+      possibleRowMatch[2].forEach(index => balloonsArray[index].src = "");
+    }
+    else if (possibleRowMatch[1].every(index => balloonsArray[index].src === matchingBalloon)) {
+      possibleRowMatch[1].forEach(index => balloonsArray[index].src = "");
+    }
+    else if (possibleRowMatch[0].every(index => balloonsArray[index].src === matchingBalloon)) {
+      possibleRowMatch[0].forEach(index => balloonsArray[index].src = "");
+    }
+  }
+}
+
+function checkForColumnMatching() {
+  for (i = 0; i < 20; i++) {
+    let matchingBalloon = balloonsArray[i].src;
+    let possibleColumnMatch = [[i, i + rowWidth, i + rowWidth * 2]];
+
+    if (possibleColumnMatch[0].every(index => balloonsArray[index].src === matchingBalloon)) {
+      possibleColumnMatch[0].forEach(index => balloonsArray[index].src = "");
+    }
+  }
+}
+
 function updateScore() {
   let scoreCount = document.getElementById("score-count");
-  scoreCount.innerHTML = `<p>Score: ${(score++) * 10}</p>`;
+  scoreCount.innerHTML = `<p>Score: ${(score = score + 10)}</p>`;
 }
 
 function startTimer(duration) {
@@ -73,63 +162,6 @@ function startTimer(duration) {
     seconds = seconds < 10 ? "0" + seconds : seconds;
     countdown.innerHTML = `<p>${minutes}:${seconds}</p>`;
     --timer;
-    if (timer < 0) {
-      clearInterval(counter);
-    }
+    if (timer < 0) clearInterval(counter);
   }, 1000);
 };
-
-function renderBalloons() {
-  for (let i = 0; gameContainer.childElementCount < 40; i++) {
-    if (gameContainer.childElementCount < 40) {
-      balloon = pickRandomBalloon();
-      balloon.id = i;
-      balloons.push(balloon);
-    }
-  }
-
-  balloons.forEach(balloon => balloon.addEventListener("dragstart", dragStart));
-  balloons.forEach(balloon => balloon.addEventListener("dragover", dragOver));
-  balloons.forEach(balloon => balloon.addEventListener("drop", dragDrop));
-}
-
-// occurs when the user starts to drag an element
-function dragStart() {
-  balloonDragged = this.src;
-  balloonDraggedId = parseInt(this.id);
-}
-
-// occurs when the dragged element is over the drop target
-function dragOver(e) {
-  e.preventDefault();
-}
-
-// occurs when the dragged element is dropped on the drop target
-function dragDrop() {
-  balloonReplaced = this.src;
-  balloonReplacedId = parseInt(this.id);
-  validMove();
-}
-
-// occurs when the user has finished dragging the element;
-// Define valid moves;
-function validMove() {
-  let validMoves = [
-    balloonDraggedId - 1,
-    balloonDraggedId - 10,
-    balloonDraggedId + 1,
-    balloonDraggedId + 10
-  ];
-  let validMove = validMoves.includes(balloonReplacedId);
-
-  if (balloonReplacedId && validMove) {
-    balloons[balloonReplacedId].src = balloonDragged;
-    balloons[balloonDraggedId].src = balloonReplaced;
-  } else if (balloonReplacedId && !validMove) {
-    balloons[balloonReplacedId].src = balloonReplaced;
-    balloons[balloonDraggedId].src = balloonDragged;
-  } else {
-    balloons[balloonDraggedId].src = balloonDragged;
-  }
-}
-
