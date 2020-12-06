206bae21a998093fd6c7994e1c26992411a2e58c
Indre Bakeer <indre.bakeer@gmail.com>
2020-12-05 18:13:20 -0600

    add games file to components dir and have Games class handle the load of all games and also render top games - have Game class only handle the instantiation of a single new Game instead

---
+++ src/components/games.js
@@ -0,0 +1,52 @@
+class Games {
+  constructor() {
+    this.games = [];
+    this.adapter = new GamesAdapter();
+    this.fetchAndLoadGames();
+  }
+
+  fetchAndLoadGames() {
+    this.adapter.getGames()
+      .then(games => {
+        games.forEach(game => this.games.push(new Game(
+          game.id,
+          game.user.username,
+          game.score
+        )));
+      })
+      .then(() => {
+        this.games.forEach(game => {
+          this.renderTopGames(game);
+        });
+      });
+  }
+
+  renderTopGames(game) {
+    const tBody = document.getElementById("table-body");
+    let tr = document.createElement("tr");
+    tr.innerHTML = `<td></td><td>${game.username}</td><td>${game.score}</td>`;
+
+    if (tBody.hasChildNodes()) {
+      (tBody.firstChild.id < game.score) ? tBody.prepend(tr) : tBody.append(tr);
+    }
+    else tBody.prepend(tr);
+
+    this.assignPositionNumber(tBody);
+  }
+
+  ordinalNumber(num) {
+    return ["st", "nd", "rd"][((num + 90) % 100 - 10) % 10 - 1] || "th";
+  }
+
+  assignPositionNumber(tBody) {
+    const rows = Array.from(tBody.getElementsByTagName("tr"));
+
+    for (let i = 0; i < tBody.childElementCount; i++) {
+      rows.forEach(row => {
+        let position = row.sectionRowIndex + 1;
+        row.firstChild.innerHTML = position + this.ordinalNumber(position);
+      });
+    }
+  }
+
+}
