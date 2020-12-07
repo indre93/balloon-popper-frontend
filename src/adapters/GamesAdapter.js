57f961b397220dbbcbc260ea3690baaaa0a3f22c
Indre Bakeer <indre.bakeer@gmail.com>
2020-12-07 12:19:44 -0600

    remove unnecessary files

--- src/adapters/GamesAdapter.js
+++
@@ -1,9 +0,0 @@
-class GamesAdapter {
-  constructor() {
-    this.baseUrl = "http://localhost:3000/games";
-  }
-
-  getGames() {
-    return fetch(this.baseUrl).then(response => response.json());
-  }
-}
--- src/adapters/UsersAdapter.js
+++
@@ -1,9 +0,0 @@
-class UsersAdapter {
-  constructor() {
-    this.baseUrl = "http://localhost:3000/users";
-  }
-
-  getUsers() {
-    return fetch(this.baseUrl).then(response => response.json());
-  }
-}
