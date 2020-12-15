29b154595b1c28d10f387a024dfe4d713252524a
Indre Bakeer <indre.bakeer@gmail.com>
2020-12-14 19:47:14 -0600

    remove updateAction method and add the code needed to show the popping img effect into the popBalloon method. Also update all the matching methods a conditional to make sure balloon has not been popped before attempting to execute popBalloon method

--- src/components/game.js
+++ src/components/game.js
@@ -7,25 +7,6 @@ class Game {
 
   begin() {
     renderBalloons(balloonImages, 2, false);
-    this.checkForMatchingBalloons();
-    this.updateAction();
-  }
-
-  updateAction() {
-    setInterval(() => {
-      if (balloons.every(balloon => balloon.getAttribute("popped") === "false")) {
-        this.checkForMatchingBalloons();
-      } else {
-        balloons.forEach(balloon => {
-          if (balloon.getAttribute("popped") === "true") {
-            updateGameTarget(balloon);
-            scoreCount.innerHTML = `<p>Score: ${(score += 10)}</p>`;
-            balloon.setAttribute("popped", "null");
-            balloon.src = "";
-          }
-        });
-      }
-    }, 100);
   }
 
   moveBalloonsUp() {
@@ -37,12 +18,22 @@ class Game {
     }
   }
 
+
   popBalloon(matchingArray, matchingBalloon) {
     if (matchingArray.every(index => balloons[index].src === matchingBalloon.src)) {
       matchingArray.forEach(index => {
         popSound.play();
         balloons[index].src = "images/pop.png";
         balloons[index].setAttribute("popped", "true");
+
+        setTimeout(() => {
+          if (balloons[index].getAttribute("popped") === "true") {
+            updateGameTarget(balloons[index]);
+            scoreCount.innerHTML = `<p>Score: ${(score += 10)}</p>`;
+            balloons[index].setAttribute("popped", "null");
+            balloons[index].src = "";
+          }
+        }, 100);
       });
     }
   }
@@ -54,13 +45,14 @@ class Game {
     this.checkForFourColumnMatching();
     this.checkForThreeColumnMatching();
   }
-
   checkForThreeRowMatch() {
     for (let i = 0; i <= 37; i++) {
       let possibleMatch = [i, i + 1, i + 2];
       let notValid = [8, 9, 18, 19, 28, 29];
       if (notValid.includes(i)) continue;
-      this.popBalloon(possibleMatch, balloons[i]);
+      if (balloons[i].getAttribute("popped") === "false") {
+        this.popBalloon(possibleMatch, balloons[i]);
+      }
     }
   }
 
@@ -69,7 +61,9 @@ class Game {
       let possibleMatch = [i, i + 1, i + 2, i + 3];
       let notValid = [7, 8, 9, 17, 18, 19, 27, 28, 29];
       if (notValid.includes(i)) continue;
-      this.popBalloon(possibleMatch, balloons[i]);
+      if (balloons[i].getAttribute("popped") === "false") {
+        this.popBalloon(possibleMatch, balloons[i]);
+      }
     }
   }
 
@@ -78,21 +72,27 @@ class Game {
       let possibleMatch = [i, i + 1, i + 2, i + 3, i + 4];
       let notValid = [6, 7, 8, 9, 16, 17, 18, 19, 26, 27, 28, 29];
       if (notValid.includes(i)) continue;
-      this.popBalloon(possibleMatch, balloons[i]);
+      if (balloons[i].getAttribute("popped") === "false") {
+        this.popBalloon(possibleMatch, balloons[i]);
+      }
     }
   }
 
   checkForThreeColumnMatching() {
     for (let i = 0; i <= 19; i++) {
       let possibleMatch = [i, i + 10, i + 10 * 2];
-      this.popBalloon(possibleMatch, balloons[i]);
+      if (balloons[i].getAttribute("popped") === "false") {
+        this.popBalloon(possibleMatch, balloons[i]);
+      }
     }
   }
 
   checkForFourColumnMatching() {
     for (let i = 0; i <= 9; i++) {
       let possibleMatch = [i, i + 10, i + 10 * 2, i + 10 * 3];
-      this.popBalloon(possibleMatch, balloons[i]);
+      if (balloons[i].getAttribute("popped") === "false") {
+        this.popBalloon(possibleMatch, balloons[i]);
+      }
     }
   }
 
