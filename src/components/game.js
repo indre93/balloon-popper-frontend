254968637b90548f7d3d87186170fb1bf10a71ca
Indre Bakeer <indre.bakeer@gmail.com>
2020-12-17 23:51:57 -0600

    include userId paramater in Game class

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,7 @@
 class Game {
-  constructor(id, username, score) {
+  constructor(id, userId, username, score) {
     this.id = id;
+    this.userId = userId;
     this.username = username;
     this.score = score;
   }
